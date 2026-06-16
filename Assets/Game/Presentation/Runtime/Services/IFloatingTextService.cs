db60773c94c96d881bb6ae4556ae78af1d1e83d0
Tang Jin <156814864+tja688@users.noreply.github.com>
2026-06-16 20:09:47 +0800

    清理代码

--- Assets/Game/Presentation/Runtime/Services/IFloatingTextService.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Game.Presentation.Services
-{
-    public interface IFloatingTextService
-    {
-        void Show(Vector3 worldPosition, string text, Color color, float duration = 1f);
-    }
-}
